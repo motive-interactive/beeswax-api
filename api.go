46441ff2c608ba497937516b56d38608161d618c
tyler <tcornell05@gmail.com>
2020-06-15 18:06:40 -0700

    testing

--- api.go
+++ api.go
@@ -1,11 +1,21 @@
-package motive_beeswax_api
+package main
 
 import (
-	_"./beeswax/adgroup"
-	_"./beeswax/augment"
-	_"./beeswax/base"
-	_"./beeswax/bid"
-	_"./beeswax/logs"
-	_"./beeswax/openrtb"
-	
+	adGroup "./beeswax/adgroup"
+	augment "./beeswax/augment"
+	base "./beeswax/base"
+	bid "./beeswax/bid"
+	logs "./beeswax/logs"
+	openrtb "./beeswax/openrtb"
+
 )
+
+func main() {
+	_ = adGroup.GhostBidding{}
+	_ = augment.AugmentorRequest{}
+	_ = base.AdGroupId{}
+	_ = bid.Adcandidate{}
+	_ = logs.ActivityLogMessage{}
+	_ = openrtb.AdvertiserInfo{}
+
+}
